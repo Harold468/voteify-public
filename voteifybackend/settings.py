6e57049a765e197a69e56566dc6ee2c57b9cc097
Harold468 <haroldosei24@gmail.com>
2024-09-27 09:03:49 +0000

    allowed hosts updated

--- voteifybackend/settings.py
+++ voteifybackend/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','.now.sh','localhost']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','.now.sh','localhost','voteify-public.onrender.com']
 
 
 # Application definition
