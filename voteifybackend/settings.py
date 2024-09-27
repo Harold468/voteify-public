5e3cf1bb4082cb2f295cb6a11fbfaf1d8e40dddd
Harold468 <haroldosei24@gmail.com>
2024-09-27 09:04:41 +0000

    static root updated

--- voteifybackend/settings.py
+++ voteifybackend/settings.py
@@ -166,8 +166,8 @@ MEDIA_ROOT = ""
 # else:
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR/"staticfiles_build"/"static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = BASE_DIR/"staticfiles_build"/"static"
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
